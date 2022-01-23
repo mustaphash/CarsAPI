e726d9688085ecf9b9f3c225b4c01e0dd2ed8f59
Mustafa Shakir <79004067+mustaphash@users.noreply.github.com>
2022-01-23 19:57:17 +0200

    Add files via upload

---
+++ src/Core/Entity/Car.cs
@@ -0,0 +1,14 @@
+﻿namespace Core.Entity
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Kilometers { get; set; }
+        public string Transmission { get; set; }
+        public string Color { get; set; }
+        public string Fuel { get; set; }
+        public DateTime DateOfManufacture { get; set; }
+    }
+}
---
+++ src/Core/Entity/Owner.cs
@@ -0,0 +1,14 @@
+﻿namespace Core.Entity
+{
+    public class Owner
+    {
+        public Owner()
+        {
+        }
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+        public DateTime PurchaseDate { get; set; }
+    }
+}
